d272ea161fb8fb62ff6d4f82177685c4e8e22a2d
EienMosu <82905592+EienMosu@users.noreply.github.com>
2021-07-19 22:46:31 +0200

    Update API.js

--- src/API.js
+++ src/API.js
@@ -3,17 +3,7 @@ import {
   POPULAR_BASE_URL,
   API_URL,
   API_KEY,
-  REQUEST_TOKEN_URL,
-  LOGIN_URL,
-  SESSION_ID_URL
 } from './config';
-
-const defaultConfig = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-};
 
 const apiSettings = {
   fetchMovies: async (searchTerm, page) => {
@@ -30,47 +20,6 @@ const apiSettings = {
     const creditsEndpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
     return await (await fetch(creditsEndpoint)).json();
   },
-  // Bonus material below for login
-  getRequestToken: async () => {
-    const reqToken = await (await fetch(REQUEST_TOKEN_URL)).json();
-    return reqToken.request_token;
-  },
-  authenticate: async (requestToken, username, password) => {
-    const bodyData = {
-      username,
-      password,
-      request_token: requestToken
-    };
-    // First authenticate the requestToken
-    const data = await (
-      await fetch(LOGIN_URL, {
-        ...defaultConfig,
-        body: JSON.stringify(bodyData)
-      })
-    ).json();
-    // Then get the sessionId with the requestToken
-    if (data.success) {
-      const sessionId = await (
-        await fetch(SESSION_ID_URL, {
-          ...defaultConfig,
-          body: JSON.stringify({ request_token: requestToken })
-        })
-      ).json();
-      return sessionId;
-    }
-  },
-  rateMovie: async (sessionId, movieId, value) => {
-    const endpoint = `${API_URL}movie/${movieId}/rating?api_key=${API_KEY}&session_id=${sessionId}`;
-
-    const rating = await (
-      await fetch(endpoint, {
-        ...defaultConfig,
-        body: JSON.stringify({ value })
-      })
-    ).json();
-
-    return rating;
-  }
 };
 
 export default apiSettings;
