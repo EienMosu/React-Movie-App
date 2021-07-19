11cbd5f4eb3ffd9c0593e6a4b1fd799d4afe3997
EienMosu <82905592+EienMosu@users.noreply.github.com>
2021-07-19 22:46:49 +0200

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,9 @@
-// Convert time to hours and minutes
 export const calcTime = time => {
   const hours = Math.floor(time / 60);
   const mins = time % 60;
   return `${hours}h ${mins}m`;
 };
-// Convert a number to money formatting
+
 export const convertMoney = money => {
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
